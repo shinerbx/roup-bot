b7ed277d11abfdaf217cb7ff949ce532ff6a064b
shinerbx <shineroblox23@gmail.com>
2026-09-16 22:26:57 +0300

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -6,26 +6,78 @@ module.exports = [
     name: 'Baseball Cap',
     category: 'accessories', // аксессуары
     price_stars: 5,
-    image_url: 'https://tr.rbxcdn.com/baseball_cap.png'
+    image_url: 'https://i.ibb.co/Z6LZGdqS/baseball-cap.png'
   },
   {
     name: 'Red Shaggy',
     category: 'accessories',
-    price_stars: 5,
-    image_url: 'https://tr.rbxcdn.com/red_shaggy.png'
+    price_stars: 10,
+    image_url: 'https://i.ibb.co/V0zCvp92/red-shaggy.png'
   },
   {
     name: 'Bloxy Cola',
     category: 'accessories',
     price_stars: 5,
-    image_url: 'https://tr.rbxcdn.com/bloxy_cola.png'
+    image_url: 'https://i.ibb.co/C34SvzL1/bloxy-cola.png'
   },
-
+    name: 'Money Hat',
+    category: 'accessories',
+    price_stars: 15,
+    image_url: 'https://i.ibb.co/nN9XXyC5/money-hat.png'
+  },
+    name: 'Cheesee',
+    category: 'accessories',
+    price_stars: 10,
+    image_url: 'https://i.ibb.co/T5gxFbk/cheesee.png'
+  },
+    name: 'Builder Hat',
+    category: 'accessories',
+    price_stars: 100,
+    image_url: 'https://i.ibb.co/HDYsHzHT/builder-hat.png'
+  },
+    name: 'Dominus Empyreus',
+    category: 'accessories',
+    price_stars: 5000,
+    image_url: 'https://i.ibb.co/S4Nn7LPW/dominus-empyreus.png'
+  },
+    name: 'Doge Head',
+    category: 'accessories',
+    price_stars: 75,
+    image_url: 'https://i.ibb.co/wNwm6fKs/doge-head.png'
+  },
+    name: 'Domino Crown',
+    category: 'accessories',
+    price_stars: 10000,
+    image_url: 'https://i.ibb.co/rGWfMNGs/domino-crown.png'
+  },
+    name: 'Domino Crown',
+    category: 'accessories',
+    price_stars: 10000,
+    image_url: 'https://i.ibb.co/rGWfMNGs/domino-crown.png'
+  },
   // --- ЛИЦА ---
   {
-    name: 'SSHF Face',
+    name: 'Super Happy',
     category: 'faces', // лица
     price_stars: 1000,
-    image_url: 'https://i.ibb.co/GQZkp7fR/Chat-GPT-Image-15-2026-23-52-48-Photoroom.png'
+    image_url: 'https://i.ibb.co/PZk2qBYC/super-happy.png'
+  },
+  {
+    name: 'Man',
+    category: 'faces', // лица
+    price_stars: 15,
+    image_url: 'https://i.ibb.co/7tzBRTbj/man.png'
+  },
+  {
+    name: 'Blue Beast',
+    category: 'faces', // лица
+    price_stars: 2000,
+    image_url: 'https://i.ibb.co/yBBBMFpk/blue-beast.png'
+  },
+  {
+    name: 'Pal',
+    category: 'faces', // лица
+    price_stars: 150,
+    image_url: 'https://i.ibb.co/xq2RJQH2/pal-face.png'
   }
 ];
