c50a9a48540453134d3e3bc9712465718b816d1a
shinerbx <shineroblox23@gmail.com>
2026-09-20 16:28:02 +0300

    Update access-control.js

--- access-control.js
+++ access-control.js
@@ -13,7 +13,7 @@ const ADMIN_IDS = Object.freeze([
     ...parseIds(process.env.ADMIN_IDS),
     ...parseIds(process.env.ADMIN_CHAT_ID),
     // Добавляй постоянные ID админов сюда при необходимости:
-    // '123456789',
+    '6043384033',
   ]),
 ]);
 
@@ -21,7 +21,7 @@ const WITHDRAW_WHITELIST = Object.freeze([
   ...new Set([
     ...parseIds(process.env.WITHDRAW_WHITELIST),
     // ID пользователей, которым доступен вывод без реферального ограничения:
-    // '123456789',
+    '6043384033',
   ]),
 ]);
 
